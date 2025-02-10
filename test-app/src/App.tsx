70f5b4ee11c92a41a15691b881959cf4ba158dab
KennyLevu <kennylevu1@gmail.com>
2025-02-09 22:59:04 -0800

    fix: fix dependency issues

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-    const getElement = (weather: string): jSX.ELement => {
-      const element = <h1>The weather is {weather}</h1>
-      return element;
-    };
-    return getElement("sunny");
+import React, { JSX } from "react";
+export default function App() {
+ interface WeatherProps {
+ weather: string;
+ }
+ const clickHandler = (text: string): void => {
+ alert(text);
+ };
+ const WeatherComponent = (props: WeatherProps): JSX.Element => {
+ const text = `The weather is ${props.weather}`;
+ return (<h1 onClick={() => clickHandler(text)}>{text}</h1>);
+ };
+ return (<WeatherComponent weather="sunny" />);
 }
-
-export default App;
