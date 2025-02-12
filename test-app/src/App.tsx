0b19bd12690dbb72ada29834bf9cf7c4944d44d8
Kenny <kennylevu1@gmail.com>
2025-02-12 00:26:45 -0800

    feat: changed ui to use class components rather than functional

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,14 +1,29 @@
-import React, { JSX } from "react";
+import React from "react";
 export default function App() {
- interface WeatherProps {
- weather: string;
- }
- const clickHandler = (text: string): void => {
- alert(text);
- };
- const WeatherComponent = (props: WeatherProps): JSX.Element => {
- const text = `The weather is ${props.weather}`;
- return (<h1 onClick={() => clickHandler(text)}>{text}</h1>);
- };
- return (<WeatherComponent weather="sunny" />);
+  interface WeatherProps {
+      weather: string;
+  }
+  type WeatherState = {
+    count: number;
+  };
+  class WeatherComponent extends React.Component<WeatherProps, WeatherState> {
+    constructor(props: WeatherProps) {
+        super(props);
+        this.state = { count: 0 };
+    }
+    componentDidMount() {
+        this.setState({ count: 1 });
+    }
+    clickHandler(): void {
+        this.setState({ count: this.state.count + 1 });
+    }
+    render() {
+      return (
+        <h1 onClick={() => this.clickHandler()}>
+            The weather is {this.props.weather}, and the counter shows{" "}
+            {this.state.count}
+        </h1> );
+    } 
+  }
+  return (<WeatherComponent weather="sunny" />);
 }
