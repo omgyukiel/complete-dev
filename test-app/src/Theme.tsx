c1d19d632a717d77e9c81798532fa645e653fd4c
Kenny <kennylevu1@gmail.com>
2025-02-12 01:21:49 -0800

    feat: add react component using context provider

---
+++ test-app/src/Theme.tsx
@@ -0,0 +1,22 @@
+import React, { useState, createContext, useContext, JSX } from 'react';
+export default function App() {
+  const ThemeContext = createContext('');
+  const ContextComponent = (): JSX.Element => {
+    const [theme, setTheme] = useState('dark');
+    return (
+      <div>
+        <ThemeContext.Provider value={theme}>
+          <button onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}>
+            Toggle theme
+          </button>
+          <Headline />
+        </ThemeContext.Provider>
+      </div>
+    );
+  };
+  const Headline = (): JSX.Element => {
+    const theme = useContext(ThemeContext);
+    return <h1 className={theme}>Current theme: {theme}</h1>;
+  };
+  return <ContextComponent />;
+}
