7630cba613332d13620e4a4c564281e0cef75825
kennylevu <kennylevu1@gmail.com>
2025-03-10 22:12:22 -0700

    feat: add next/head component for seo

--- sample-next/my-app/src/pages/hello.tsx
+++ sample-next/my-app/src/pages/hello.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next';
+import Head from "next/head";
 
 const Hello: NextPage = () => {
-    return (<div className="hello">Hello World!</div>);
-}
+    return (
+        <div>
+            <Head>
+                <title>Hello SEO World</title>
+                <meta  property="og:title" content="Hello World" key="title" />
+            </Head>
+            <div className="hello">Hello World!</div>);
+        </div>
+    );
+};
 
 export default Hello;
