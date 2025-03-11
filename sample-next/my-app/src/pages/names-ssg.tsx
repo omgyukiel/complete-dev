cd5cd441c9faf4c76c311c675622ddebff4da1f1
kennylevu <kennylevu1@gmail.com>
2025-03-11 00:02:10 -0700

    feat: add static site generated page

---
+++ sample-next/my-app/src/pages/names-ssg.tsx
@@ -0,0 +1,47 @@
+import type {
+    GetStaticProps,
+    GetStaticPropsContext,
+    InferGetStaticPropsType,
+    NextPage,
+    PreviewData
+} from "next";
+import {ParsedUrlQuery} from "querystring";
+import {fetchNames} from "../utils/fetch-names";
+
+type responseItemType = {
+    id: string;
+    name: string;
+};
+
+const NameSSG: NextPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const output = props.names.map((item: responseItemType, idx: number) => {
+        return (
+            <li key={`name-${idx}`}>
+                {item.id} : {item.name}
+            </li>
+        )
+    })
+
+    return (
+        <ul>
+            {output}
+        </ul>
+    )
+}
+// marked as ssg when exporting getStaticProps
+export const getStaticProps: GetStaticProps = async (
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    context: GetStaticPropsContext<ParsedUrlQuery, PreviewData>
+) => {
+    let names: responseItemType[] | [] = [];
+    try {
+        names = await fetchNames();
+    } catch(err) { console.log(err) }
+    return {
+        props: {
+            names
+        }
+    };
+};
+
+export default NameSSG;
