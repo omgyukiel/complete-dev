c66b980aa880053a5a99de739ac7fd81784cf345
kennylevu <kennylevu1@gmail.com>
2025-03-10 22:26:52 -0700

    feat: add html anchor and nextjs link component

--- sample-next/my-app/src/pages/hello.tsx
+++ sample-next/my-app/src/pages/hello.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from "next/head";
+import Link from "next/link";
 
 const Hello: NextPage = () => {
     return (
@@ -8,7 +9,13 @@ const Hello: NextPage = () => {
                 <title>Hello SEO World</title>
                 <meta  property="og:title" content="Hello World" key="title" />
             </Head>
-            <div className="hello">Hello World!</div>);
+            <div className="hello">Hello World!</div>
+            <div>
+                Use the HTML anchor for an
+                <a className="hello" href="https://nostarch.com" > external link </a>
+                and the link component for an
+                <Link className="hello" href="/components/weather"> internal page</Link>.
+            </div>
         </div>
     );
 };
