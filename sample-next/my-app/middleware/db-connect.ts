08a8bc97251db790d97970a4281c0d9ef144b646
kennylevu <kennylevu1@gmail.com>
2025-03-20 22:03:51 -0700

    feat: add e2e get by zipcode with seeded db

--- sample-next/my-app/middleware/db-connect.ts
+++ sample-next/my-app/middleware/db-connect.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import { storeDocument } from "../mongoose/weather/service";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function dbConnect(): Promise<any | string> {
@@ -9,6 +10,29 @@ async function dbConnect(): Promise<any | string> {
     await mongoose.connect(MONGOIO_URI, {
         dbName: "Weather"
     });
+
+    await storeDocument({
+        zip: "96815",
+        weather: "sunny",
+        tempC: "25C",
+        tempF: "70F",
+        friends: ["96814", "96826"]
+    });
+    await storeDocument({
+        zip: "96814",
+        weather: "rainy",
+        tempC: "20C",
+        tempF: "68F",
+        friends: ["96815", "96826"]
+    });
+    await storeDocument({
+        zip: "96826",
+        weather: "rainy",
+        tempC: "30C",
+        tempF: "86F",
+        friends: ["96815", "96814"]
+    });
+    
 }
 
 export default dbConnect;
--- sample-next/my-app/src/pages/api/v1/weather/[zipcode].ts
+++ sample-next/my-app/src/pages/api/v1/weather/[zipcode].ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { findByZip } from "../../../../../mongoose/weather/service";
+import dbConnect from "../../../../../middleware/db-connect";
+dbConnect();
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ): Promise<NextApiResponse<WeatherDetailType> | void> {
-    return res.status(200).json({
-        zipcode: req.query.zipcode,
-        weather: "sunny",
-        temp: 35
-    });
+    // console.log(req.query);
+    const data = await findByZip(req.query.zipcode as string);
+    return res.status(200).json(data);
 }
